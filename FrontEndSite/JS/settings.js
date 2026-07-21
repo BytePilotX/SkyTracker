2e181ed92ed97b1c265061bbf0ff8d8b6356f5f8
VastSkys <100413904+BytePilotX@users.noreply.github.com>
2026-07-21 23:21:33 +0100

    Adding _redirects and pushing a semi-finished update

--- FrontEndSite/JS/settings.js
+++ FrontEndSite/JS/settings.js
@@ -1,9 +1,21 @@
 let appearenceCheck = document.getElementById("appearance-checkbox");
+const appearanceCheckBox = document.getElementById("appearance-checkbox");
+var storedAppearance = localStorage.getItem("appearance");
+
+if (!storedAppearance) {
+    console.log("NO STORED APP");
+    appearanceCheckBox.checked = false;
+} else if (storedAppearance === "dark") {
+    document.body.setAttribute("data-theme", "dark");
+    appearanceCheckBox.checked = true;
+}
 
 appearenceCheck.addEventListener("change", function() {
     if (this.checked) {
        document.body.setAttribute("data-theme", "dark");
+       localStorage.setItem("appearance", "dark");
     } else {
          document.body.setAttribute("data-theme", "light");
+         localStorage.setItem("appearance", "light");
     }
 });
