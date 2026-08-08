0cf784b86b1abad49cbb6327db61433d86020e44
VastSkys <100413904+BytePilotX@users.noreply.github.com>
2026-08-08 21:04:52 +0100

    Major bug fix and update
    Fixed the main bug with the map not loading until a screen re-size and also fixed issues with the loading screen and light and dark mode. Added the flights.html page which will now allow me to eventually make a start on the backend with basic functionality now available. Expect further aesthetic changes.

--- FrontEndSite/JS/loading.js
+++ FrontEndSite/JS/loading.js
@@ -1,8 +1,11 @@
 function onPageLoad() {
     window.addEventListener('load', showPage());
+    //setTimeout(showPage, 2000); // This line is used for loading screen development purposes.
 }
 
 function showPage() {
     document.getElementById("loading_screen").style.display = "none";
-    document.getElementById("home_page").style.display = "block";
+    setTimeout(function(){
+        document.getElementById("main_page").style.visibility = "visible";
+    }, 100);
 }
--- FrontEndSite/JS/settings.js
+++ FrontEndSite/JS/settings.js
@@ -8,14 +8,27 @@ if (!storedAppearance) {
 } else if (storedAppearance === "dark") {
     document.body.setAttribute("data-theme", "dark");
     appearanceCheckBox.checked = true;
+    console.log("Appearance set to dark (Found in localStorage)");
+} else if (storedAppearance === "light") {
+    document.body.setAttribute("data-theme", "light");
+    appearanceCheckBox.checked = false;
+    console.log("Appearance set to light (Found in localStorage)");
 }
 
 appearenceCheck.addEventListener("change", function() {
     if (this.checked) {
        document.body.setAttribute("data-theme", "dark");
+       document.getElementById("loading_screen").setAttribute("data-theme", "dark");
+       document.getElementById("loading").setAttribute("data-theme", "dark");
+       document.getElementById("title").setAttribute("data-theme", "dark"); // Needs work
        localStorage.setItem("appearance", "dark");
+       console.log("Appearance set to dark");
     } else {
          document.body.setAttribute("data-theme", "light");
+         document.getElementById("loading_screen").setAttribute("data-theme", "light");
+         document.getElementById("loading").setAttribute("data-theme", "light");
+         document.getElementById("title").setAttribute("data-theme", "light"); // Needs work
          localStorage.setItem("appearance", "light");
+         console.log("Appearance set to light");
     }
 });
